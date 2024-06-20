e84faef2a5c2f6a9c5a6a4db75d44c7477714ab1
lew663 <leunwoo000@gmail.com>
2024-06-20 18:36:32 +0900

    1. 프로젝트 생성

---
+++ src/main/java/zerobase/wifi/WifiApplication.java
@@ -0,0 +1,12 @@
+package zerobase.wifi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WifiApplication.class, args);
+	}
+}
